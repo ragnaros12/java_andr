c65cbcf0e352e095d5b85e9313de91de692f40f2
ragnaros12 <72325821+ragnaros12@users.noreply.github.com>
2021-02-01 15:10:37 +0300

    dd

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.RequestPoint;
+import com.yandex.mapkit.RequestPointType;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.geometry.Polyline;
+import com.yandex.mapkit.geometry.SubpolylineHelper;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.PolylineMapObject;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.search.POIObjectMetadata;
+import com.yandex.mapkit.transport.TransportFactory;
+import com.yandex.mapkit.transport.masstransit.MasstransitOptions;
+import com.yandex.mapkit.transport.masstransit.MasstransitRouter;
+import com.yandex.mapkit.transport.masstransit.PedestrianRouter;
+import com.yandex.mapkit.transport.masstransit.Route;
+import com.yandex.mapkit.transport.masstransit.Section;
+import com.yandex.mapkit.transport.masstransit.SectionMetadata;
+import com.yandex.mapkit.transport.masstransit.Session;
+import com.yandex.mapkit.transport.masstransit.TimeOptions;
+import com.yandex.mapkit.transport.masstransit.Transport;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
+
+import java.security.Permission;
+import java.security.Permissions;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Session.RouteListener {
+
+    private final String MAPKIT_API_KEY = "9a82ebf5-58a5-4e4a-9c05-ca3a05df40df";
+    private MapView mapView;
+    private MapObjectCollection mapObjects;
+    private PedestrianRouter mtRouter;
+    ArrayList<Point> p = new ArrayList<Point>();
+    int index = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+        else {
+            p.add(new Point(54.769679, 32.095323));
+            MapKitFactory.setApiKey(MAPKIT_API_KEY);
+            MapKitFactory.initialize(this);
+            TransportFactory.initialize(this);
+            setContentView(R.layout.activity_main);
+            super.onCreate(savedInstanceState);
+            mapView = findViewById(R.id.mapview);
+            LocationListener l = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    if(Math.sqrt(Math.pow(location.getLatitude() - p.get(index).getLatitude(),2) + Math.pow(location.getLongitude() - p.get(index).getLongitude(),2)) <= 0.0005){
+                        index++;
+                    }
+                    draw(new Point(location.getLatitude(), location.getLongitude()), p.get(index));
+                    mapView.getMap().move(
+                            new CameraPosition(new Point(location.getLatitude(),location.getLongitude()), 12.0f, 0.0f, 0.0f),
+                            new Animation(Animation.Type.SMOOTH, 5),
+                            null);
+                }
+            };
+            LocationManager ma = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            ma.requestLocationUpdates(ma.GPS_PROVIDER, 0, 0, l);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onMasstransitRoutes(List<Route> routes) {
+        // In this example we consider first alternative only
+        if (routes.size() > 0) {
+            for (Section section : routes.get(0).getSections()) {
+                drawSection(
+                        section.getMetadata().getData(),
+                        SubpolylineHelper.subpolyline(
+                                routes.get(0).getGeometry(), section.getGeometry()));
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+    }
+
+    @Override
+    public void onMasstransitRoutesError(Error error) {
+        String errorMessage = getString(R.string.unknown_error_message);
+        if (error instanceof RemoteError) {
+            errorMessage = getString(R.string.remote_error_message);
+        } else if (error instanceof NetworkError) {
+            errorMessage = getString(R.string.network_error_message);
+        }
+
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void drawSection(SectionMetadata.SectionData data, Polyline geometry) {
+        PolylineMapObject polylineMapObject = mapObjects.addPolyline(geometry);
+        polylineMapObject.setStrokeColor(Color.RED);
+    }
+
+    public void draw(Point ROUTE_START_LOCATION, Point ROUTE_END_LOCATION){
+        mapObjects = mapView.getMap().getMapObjects().addCollection();
+        List<RequestPoint> points = new ArrayList<RequestPoint>();
+        points.add(new RequestPoint(ROUTE_START_LOCATION, RequestPointType.WAYPOINT, null));
+        points.add(new RequestPoint(ROUTE_END_LOCATION, RequestPointType.WAYPOINT, null));
+        mtRouter = TransportFactory.getInstance().createPedestrianRouter();
+        mtRouter.requestRoutes(points, new TimeOptions(), this);
+    }
+}
